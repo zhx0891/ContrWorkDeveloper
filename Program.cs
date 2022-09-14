712491fb8df446e9fd267a585bd995fa8145ffa2
zhukovv <northbug@yandex.ru>
2022-09-14 12:18:48 +0500

    finish

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+string [] arr={" drawio","it","ell", "reused", "git","!!!"};
+string [] arr2 =new string[0]; 
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if(arr[i].Length <= 3)
+    {
+        count++;
+        Array.Resize(ref arr2,count);
+        arr2[count-1] =arr[i];
+        
+    }
+   
+}
+System.Console.WriteLine();
+ for (int i = 0; i <arr2.Length ; i++)
+    {
+        System.Console.Write(arr2[i] + " "); 
+    }
+
